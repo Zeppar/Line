3e1b4b294c66bcc95e7fd9154acfafe20a64a2dd
Zeppar <849166805@qq.com>
2017-05-20 18:23:29 +0800

    beta 1.0

--- Line/Assets/Scripts/Controller/GameController.cs
+++ Line/Assets/Scripts/Controller/GameController.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum DragDirection {
+	Up,
+	Down,
+	Left,
+	Right
+}
+
 public class GameController : MonoBehaviour {
 
 	public ChessItem chessPrefab;
@@ -10,10 +17,20 @@ public class GameController : MonoBehaviour {
 
 	public Transform boardTransform;
 
+	public bool hasSelectedItem = false;
+
 	//TODO
 	List<ChessItem> chessList = new List<ChessItem> (16);
 
 	public int Level = 4;
+
+	ChessItem selectItem = null;
+
+	ChessItem nearItem = null;
+
+	public int count = 0;
+
+	public bool gameStart = false;
 
 	void Awake() {
 		if (manager == null) {
@@ -27,6 +44,7 @@ public class GameController : MonoBehaviour {
 	void Start() {
 		InitChessBoard ();
 		RandomLine ();
+
 	}
 
 	void InitChessBoard() {
@@ -42,10 +60,8 @@ public class GameController : MonoBehaviour {
 		for (int i = 0; i < chessList.Count; i++) {
 			if (!chessList [i].lined) {
 				int random = Random.Range (0, Level * Level);
-				while (chessList [random].lined) {
-//					while (random == i) {
+				while (chessList [random].lined || random == i) {
 						random = Random.Range (0, Level * Level);
-//					}
 				}
 				chessList [i].lined = true;
 				chessList [random].lined = true;
@@ -55,13 +71,129 @@ public class GameController : MonoBehaviour {
 		}
 	}
 
+	void SimulateBack() {
+		int i = 0;
+		while (i < count) {
+
+			i++;
+		}
+		gameStart = true;
+	}
+
 	public void ActivateIndex(int index1, int index2) {
+		hasSelectedItem = true;
 		for (int i = 0; i < chessList.Count; i++) {
-			if (chessList [i].curItemIndex == index1 || chessList [i].curItemIndex == index2) {
+			if (chessList [i].curItemIndex == index1) {
+				chessList [i].SelectSelf ();
+				chessList [i].ActivateSelf ();
+				selectItem = chessList [i];
+			}
+			else if (chessList [i].curItemIndex == index2) {
+				chessList [i].DeselectSelf ();
 				chessList [i].ActivateSelf ();
 			} else {
+				chessList [i].DeselectSelf ();
 				chessList [i].DeactivateSelf ();
 			}
 		}
+	}
+
+	public void ConvertIndex(int index1, int index2) {
+		for (int i = 0; i < chessList.Count; i++) {
+			if (chessList [i].curItemIndex == index1 || chessList [i].curItemIndex == index2) {
+				chessList [i].Convert ();
+			}
+		}
+	}
+
+
+	void Update() {
+		if (!hasSelectedItem)
+			return;
+		if (!gameStart)
+			return;
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			if (CheckAvailable (DragDirection.Up)) {
+				nearItem = GetNearItem (DragDirection.Up);
+				if (nearItem != null) {
+					Type temp = nearItem.curType;
+					nearItem.ChangeToType (selectItem.curType);
+					selectItem.ChangeToType (temp);
+				}
+			}
+		} else if(Input.GetKeyDown(KeyCode.DownArrow)){
+			if (CheckAvailable (DragDirection.Down)) {
+				nearItem = GetNearItem (DragDirection.Down);
+				if (nearItem != null) {
+					Type temp = nearItem.curType;
+					nearItem.ChangeToType (selectItem.curType);
+					selectItem.ChangeToType (temp);
+				}
+			}
+		} else if(Input.GetKeyDown(KeyCode.LeftArrow)){
+			if (CheckAvailable (DragDirection.Left)) {
+				nearItem = GetNearItem (DragDirection.Left);
+				if (nearItem != null) {
+					Type temp = nearItem.curType;
+					nearItem.ChangeToType (selectItem.curType);
+					selectItem.ChangeToType (temp);
+				}
+			}
+		} else if(Input.GetKeyDown(KeyCode.RightArrow)){
+			if (CheckAvailable (DragDirection.Right)) {
+				nearItem = GetNearItem (DragDirection.Right);
+				if (nearItem != null) {
+					Type temp = nearItem.curType;
+					nearItem.ChangeToType (selectItem.curType);
+					selectItem.ChangeToType (temp);
+				}
+			}
+		} 
+	}
+
+	ChessItem GetNearItem(DragDirection dir) {
+		switch(dir) {
+		case DragDirection.Up:
+			return chessList[selectItem.curItemIndex - Level];
+			break;
+		case DragDirection.Down:
+			return chessList[selectItem.curItemIndex + Level];
+			break;
+		case DragDirection.Left:
+			return chessList[selectItem.curItemIndex - 1];
+			break;
+		case DragDirection.Right:
+			return chessList[selectItem.curItemIndex + 1];
+			break;
+		default:
+			break;
+		}
+		return null;
+	}
+
+	bool CheckAvailable(DragDirection dir) {
+		int row = selectItem.curItemIndex / 4;
+		int col = selectItem.curItemIndex % 4;
+		switch(dir) {
+		case DragDirection.Up:
+			if (row == 0)
+				return false;
+				break;
+		case DragDirection.Down:
+			if (row == 3)
+				return false;
+				break;
+		case DragDirection.Left:
+			if (col == 0)
+				return false;
+				break;
+		case DragDirection.Right:
+			if (col == 3)
+				return false;
+				break;
+			default:
+				break;
+		}
+		return true;
 	}
 }
--- Line/Assets/Scripts/Item/ChessItem.cs
+++ Line/Assets/Scripts/Item/ChessItem.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ChessItem : MonoBehaviour, IPointerClickHandler {
+public enum Type {
+	Cow,
+	Hen
+}
+
+public class ChessItem : MonoBehaviour, IPointerClickHandler/*, IBeginDragHandler, IEndDragHandler*/ {
 
 	//if it is lined to other items
 	public bool lined = false;
@@ -13,6 +18,12 @@ public class ChessItem : MonoBehaviour, IPointerClickHandler {
 	public Image cowImage;
 	public Image henImage;
 	public Image flagImage;
+	public Image selectImage;
+	bool isActive = false;
+	bool isSelected = false;
+	public Type curType = Type.Cow;
+	Vector2 startPos;
+	Vector2 endPos;
 
 	void Start() {
 	
@@ -24,19 +35,71 @@ public class ChessItem : MonoBehaviour, IPointerClickHandler {
 
 	public void OnPointerClick(PointerEventData data) {
 		print ("Length : " + linedItemIndexs.Count);
-		ActivateLineItem ();
+		if (!GameController.manager.gameStart)
+			return;
+		if (!isSelected) {
+			ActivateLineItem ();
+		}
+		else
+			GameController.manager.ConvertIndex (curItemIndex, linedItemIndexs [0]);
+	}
+
+//	public void OnBeginDrag(PointerEventData data) {
+//		print ("Begin");
+//		print ("Begin : " + data.position);
+//	}
+//
+//	public void OnEndDrag(PointerEventData data) {
+//		print ("End");
+//		print ("End : " + data.position);
+//	}
+
+	public void Convert() {
+		if (curType == Type.Cow) {
+			curType = Type.Hen;
+			cowImage.gameObject.SetActive (false);
+			henImage.gameObject.SetActive (true);
+		} else {
+			curType = Type.Cow;
+			cowImage.gameObject.SetActive (true);
+			henImage.gameObject.SetActive (false);
+		}
+	}
+
+	public void ChangeToType(Type t) {
+		curType = t;
+		if (curType == Type.Cow) {
+			cowImage.gameObject.SetActive (true);
+			henImage.gameObject.SetActive (false);
+		} else {
+			cowImage.gameObject.SetActive (false);
+			henImage.gameObject.SetActive (true);
+		}
 	}
 
 	public void ActivateLineItem() {
 		GameController.manager.ActivateIndex (curItemIndex, linedItemIndexs [0]);
 	}
 
+	public void SelectSelf() {
+		isSelected = true;
+		selectImage.gameObject.SetActive (true);
+	}
+
+	public void DeselectSelf() {
+		isSelected = false;
+		selectImage.gameObject.SetActive (false);
+	}
+
 	public void DeactivateSelf() {
+		isActive = false;
 		flagImage.gameObject.SetActive (false);
 	}
 
 	public void ActivateSelf() {
+		isActive = true;
 		flagImage.gameObject.SetActive (true);
 	}
 
+		
 }
