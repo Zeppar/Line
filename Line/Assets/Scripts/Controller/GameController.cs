711cecfdf39528c23cffadfe9cc2d0c54da3c365
Zeppar <849166805@qq.com>
2017-05-20 13:23:17 +0800

    0520 first commit

---
+++ Line/Assets/Scripts/Controller/GameController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public ChessItem chessPrefab;
+
+	public static GameController manager = null;
+
+	public Transform boardTransform;
+
+	//TODO
+	List<ChessItem> chessList = new List<ChessItem> (16);
+
+	public int Level = 4;
+
+	void Awake() {
+		if (manager == null) {
+			manager = this;
+			DontDestroyOnLoad (gameObject);
+		} else {
+			Destroy (gameObject);
+		}
+	}
+
+	void Start() {
+		InitChessBoard ();
+		RandomLine ();
+	}
+
+	void InitChessBoard() {
+		for (int i = 0; i < Level * Level; i++) {
+			ChessItem item = Instantiate (chessPrefab);
+			item.transform.SetParent (boardTransform, true);
+			item.curItemIndex = i;
+			chessList.Add (item);
+		}
+	}
+
+	void RandomLine() {
+		for (int i = 0; i < chessList.Count; i++) {
+			if (!chessList [i].lined) {
+				int random = Random.Range (0, Level * Level);
+				while (chessList [random].lined) {
+//					while (random == i) {
+						random = Random.Range (0, Level * Level);
+//					}
+				}
+				chessList [i].lined = true;
+				chessList [random].lined = true;
+				chessList [i].linedItemIndexs.Add (chessList [random].curItemIndex);
+				chessList [random].linedItemIndexs.Add (chessList [i].curItemIndex);
+			}
+		}
+	}
+
+	public void ActivateIndex(int index1, int index2) {
+		for (int i = 0; i < chessList.Count; i++) {
+			if (chessList [i].curItemIndex == index1 || chessList [i].curItemIndex == index2) {
+				chessList [i].ActivateSelf ();
+			} else {
+				chessList [i].DeactivateSelf ();
+			}
+		}
+	}
+}
---
+++ Line/Assets/Scripts/Item/ChessItem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ChessItem : MonoBehaviour, IPointerClickHandler {
+
+	//if it is lined to other items
+	public bool lined = false;
+	public int curItemIndex = 0;
+	public List<int> linedItemIndexs = new List<int> (4);
+	public Image cowImage;
+	public Image henImage;
+	public Image flagImage;
+
+	void Start() {
+	
+	}
+
+	void Update() {
+	
+	}
+
+	public void OnPointerClick(PointerEventData data) {
+		print ("Length : " + linedItemIndexs.Count);
+		ActivateLineItem ();
+	}
+
+	public void ActivateLineItem() {
+		GameController.manager.ActivateIndex (curItemIndex, linedItemIndexs [0]);
+	}
+
+	public void DeactivateSelf() {
+		flagImage.gameObject.SetActive (false);
+	}
+
+	public void ActivateSelf() {
+		flagImage.gameObject.SetActive (true);
+	}
+
+}
